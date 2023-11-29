88e6a57c70b76a947ece2fa99b9bcab3e5c7f1b6
NamanKumar22 <namankumar41@gmail.com>
2023-11-28 20:28:16 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,52 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>Welcome to the React Styling Assignment</h1>
+        <p>Explore and enhance your styling skills!</p>
       </header>
+
+      <section>
+        <article>
+          <h2>Article 1</h2>
+          <p>This is the content of the first article. Add your own text here.</p>
+        </article>
+
+        <article>
+          <h2>Article 2</h2>
+          <p>This is the content of the second article. Add your own text here.</p>
+        </article>
+
+        <article>
+          <h2>Article 3</h2>
+          <p>This is the content of the third article. Add your own text here.</p>
+        </article>
+      </section>
+
+      <aside className="sidebar">
+        <section>
+          <h2>Categories</h2>
+          <ul>
+            <li>Category 1</li>
+            <li>Category 2</li>
+            <li>Category 3</li>
+          </ul>
+        </section>
+
+        <section>
+          <h2>Tags</h2>
+          <ul>
+            <li>Tag 1</li>
+            <li>Tag 2</li>
+            <li>Tag 3</li>
+          </ul>
+        </section>
+      </aside>
+
+      <footer>
+        <p>&copy; 2023 React Styling Assignment</p>
+      </footer>
     </div>
   );
 }
